22597e1627a85d177a7657a6b33dd2b79c58d084
Soumyadri Das <dadsoumyadri@gmail.com>
2021-07-14 22:04:03 +0530

    React application base design created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import DenseAppBar from './Components/NavBar/NavBar';
+import { Intro } from './Components/Intro/Intro';
+import { About } from './Components/About/About';
+import { Skill } from './Components/Skill/Skill';
+import { Project } from './Components/Project/Project';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DenseAppBar />
+      <Intro />
+      <About />
+      <Skill />
+      <Project />
     </div>
   );
 }
