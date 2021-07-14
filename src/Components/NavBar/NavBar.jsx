7eba99e2fe26191d0cd021e388894cc5b5f08f94
Soumyadri Das <dadsoumyadri@gmail.com>
2021-07-14 22:11:27 +0530

    NavBar design modified & refactor code

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import styled from 'styled-components';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  }
-}));
 
 const Logo = styled.img`
     width: 70px;
@@ -17,10 +10,9 @@ const Logo = styled.img`
 `
 
 export default function DenseAppBar() {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div>
       <AppBar color="transparent" position="static">
         <Logo className="logo-image" src="https://st2.depositphotos.com/3867453/5745/v/600/depositphotos_57451535-stock-illustration-letter-s-logo-icon-design.jpg" alt="" />
       </AppBar>
